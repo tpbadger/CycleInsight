127690eb0b757f2f611b53d46abca7417e996a6c
Tom Badger <tombadger12@gmail.com>
2018-09-02 10:02:06 +0100

    added testing file and tested analogue vs digital hall effect

---
+++ testing/testing.ino
@@ -0,0 +1,20 @@
+volatile byte detects;
+
+
+void magDetect() {
+    detects++;
+    Serial.println('detected');
+}
+
+
+void setup() {
+    Serial.begin(115200);
+    attachInterrupt(digitalPinToInterrupt(2), magDetect, RISING);//Initialize the intterrupt pin for distance (Arduino digital pin 1)
+    detects = 0;
+}
+
+void loop() {
+    delay(1000);
+    Serial.print('Detected :');
+    Serial.println(detects);
+}
